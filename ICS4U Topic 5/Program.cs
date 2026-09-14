b30a1049b8784575fbb9e3fa6381ab0bbca41780
Alexofthenorth <alexofthenorth10@protonmail.com>
2026-09-14 11:55:56 -0400

    Add project files.

---
+++ ICS4U Topic 5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICS4U_Topic_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+
+        public static void Menu()
+        { 
+        
+        
+        }
+    }
+}
