559c64d2831ca550c4b8e5d4ce0bc4585514768b
Alexofthenorth <alexofthenorth10@protonmail.com>
2026-09-14 13:18:28 -0400

    Finished Compass
    Started parking

--- ICS4U Topic 5/Program.cs
+++ ICS4U Topic 5/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,14 +11,222 @@ namespace ICS4U_Topic_5
     {
         static void Main(string[] args)
         {
-
+            Menu();
 
         }
 
         public static void Menu()
+        {
+            bool exit = false;
+
+
+            string menuInput = "";
+
+            while (!exit)
+            {
+                Console.WriteLine("Menu");
+                Console.WriteLine();
+
+                Console.WriteLine("0. Exit");
+                Console.WriteLine("1. Compass Bearings");
+                Console.WriteLine("2. Parking Garage Cost");
+                Console.WriteLine("3. Hurricane");
+
+
+                Console.WriteLine();
+                Console.Write("Enter your choice: ");
+                menuInput = Console.ReadLine();
+
+                if (menuInput == "0")
+                {
+                    exit = true;
+                }
+                else if (menuInput == "1")
+                {
+                    Console.Clear();
+                    CompassBearings();
+                    Console.Clear();
+                }
+                else if (menuInput == "2")
+                {
+
+                }
+                else if (menuInput == "3")
+                {
+                
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Invalid choice. Please try again.");
+                }
+
+            }
+        }
+
+        public static void CompassBearings()
         { 
-        
-        
+            bool invalidBearing = true;
+            double bearing = 0.0;
+
+            
+
+            while (invalidBearing)
+            {
+                Console.WriteLine("Compass Bearings : ");
+                Console.WriteLine();
+
+                Console.WriteLine("Enter Bearing (in degrees): ");
+
+                if (!Double.TryParse(Console.ReadLine(), out bearing))
+                {
+                    Console.Clear();
+                    Console.WriteLine("Invalid Input");
+                }
+                else
+                {
+                    if (bearing < 0)
+                    {
+                        bearing = bearing * -1;
+                    }
+
+                    bearing = bearing % 360;
+
+                    
+
+                    if ((bearing > 315 && bearing <= 360) || (bearing < 45 && bearing >= 0)) // North
+                    {
+                        Console.Clear();
+
+                        Console.WriteLine("You are travelling North.");
+
+                        Console.WriteLine("Press any key to exit");
+
+                        Console.ReadKey();
+                    }
+                    else if (bearing > 45 && bearing < 135) // East
+                    {
+                        Console.Clear();
+
+                        Console.WriteLine("You are travelling East.");
+
+                        Console.WriteLine("Press any key to exit");
+
+                        Console.ReadKey();
+                    }
+                    else if (bearing > 135 && bearing < 225) // South
+                    {
+                        Console.Clear();
+
+                        Console.WriteLine("You are travelling South.");
+
+                        Console.WriteLine("Press any key to exit");
+
+                        Console.ReadKey();
+                    }
+                    else if (bearing > 225 && bearing < 315) // West
+                    {
+                        Console.Clear();
+
+                        Console.WriteLine("You are travelling West.");
+
+                        Console.WriteLine("Press any key to exit");
+
+                        Console.ReadKey();
+                    }
+                    else if (bearing == 45) // Northeast
+                    {
+                        Console.Clear();
+
+                        Console.WriteLine("You are travelling Northeast");
+
+                        Console.WriteLine("Press any key to exit");
+
+
+                        Console.ReadKey();
+                    
+                    }
+                    else if (bearing == 135) // Southeast
+                    {
+                        Console.Clear();
+
+                        Console.WriteLine("You are travelling Southeast");
+
+                        Console.WriteLine("Press any key to exit");
+
+
+                        Console.ReadKey();
+
+                    }
+                    else if (bearing == 225) // Southwest
+                    {
+                        Console.Clear();
+
+                        Console.WriteLine("You are travelling Southwest");
+                        Console.WriteLine("Press any key to exit");
+
+
+                        Console.ReadKey();
+
+                    }
+                    else if (bearing == 45) // Northwest
+                    {
+                        Console.Clear();
+
+                        Console.WriteLine("You are travelling Northwest");
+                        Console.WriteLine("Press any key to exit");
+
+
+                        Console.ReadKey();
+
+                    }
+                    else
+                    {
+                        Console.Clear();
+
+                        Console.WriteLine("Error");
+                        Console.WriteLine("Press any key to exit");
+
+
+                        Console.ReadKey();
+
+                    }
+
+
+                    invalidBearing = false;
+                }
+
+            }
+            
+
+
+            
+        }
+
+        public static void ParkingGarage()
+        {
+            bool finish = false;
+
+            int time = 0;
+
+            double price = 4.00;
+            double priceAditional = 2.00;
+            double priceMax = 20.00;
+
+
+            while (!finish)
+            {
+                Console.WriteLine("Parking Garage");
+                Console.WriteLine();
+                Console.WriteLine("Input time parked (minutes) : ");
+
+                if (Int32.TryParse(Console.ReadLine(), out time))
+                {
+                    time = time / 60;
+
+                    Console.WriteLine(price + (time * priceAditional));
+                }
+            }
         }
     }
 }
